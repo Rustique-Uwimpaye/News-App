c11e71b9054f31e692f782c692c91727c813dbc7
Rustique-Uwimpaye <rustique644@gmail.com>
2020-10-21 09:21:21 +0300

    css , images,tests

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,11 @@
 from flask import render_template
 from . import main
-from ..requests import news,sources,get_articles,get_sources
+from ..requests import get_articles,get_sources
 
 @main.route("/")
 def index():
-    latest_news=news("latest-headlines")
-    print(top_news)
-    source=sources("sources")
-    return render_template("index.html",latest_headline=top_news,sources=source)
+    
+    return render_template("index.html")
 
 @main.route("/articles/<name>")
 def body(name):
@@ -17,3 +15,6 @@ def body(name):
     technical=get_articles("ars-technical")
 
     return render_template("articles.html",name=name,bbc_news=bbc,abc_news=abc,aljazeera_n=aljazeera,tech=technical)
+
+def news(headlines):
+ 
--- app/requests.py
+++ app/requests.py
@@ -16,69 +16,7 @@ def configure_request(app):
     base_url=app.config["NEWS_API_BASE_URL"]
     source_url=app.config["NEWS_BASE_URL"]
     article_url=app.config["NEWS_ARTICLE_API"]
-def news(headlines):
-    get_news_url=base_url.format(headlines,api_key)
-    with urllib.request.urlopen(get_news_url) as url:
-        get_news_data=url.read()
-        get_news_response=json.loads(get_news_data)
 
-        news_results=None
-
-        if get_news_response['articles']:
-            news_result_list=get_news_response["articles"]
-            news_results=process_results(news_result_list)
-
-    return news_results
-
-def process_results(news_list):
-    news_results=[]
-    for news in news_list:
-
-        title=news.get("title")
-        description=news.get("description")
-        urltoImage=news.get("urlToImage")
-        content=news.get("content")
-        url=news.get("url")
-        if description and urltoImage:
-
-            news_object=News(title,description,urltoImage,content,url)
-
-            news_results.append(news_object)
-            news_results=news_results[:6]
-    return news_results
-'''
-Getting various sources
-'''
-def sources(sources):
-    get_news_url=source_url.format(sources,api_key)
-    with urllib.request.urlopen(get_news_url) as url:
-        source_data=url.read()
-        source_response=json.loads(source_data)
-
-        source_results=None
-
-        if source_response["sources"]:
-            new_source_results=source_response["sources"]
-            source_results=process_sources(new_source_results)
-    return source_results
-
-def process_sources(source_list):
-    source_results=[]
-    for source in source_list:
-        id=source.get("id")
-        name=source.get("name")
-        description=source.get("description")
-        url=source.get("url")
-
-        source_object=Sources(id,name,description,url)
-        source_results.append(source_object)
-        source_results=source_results[:4]
-
-    return source_results
-
-'''
-Fetching Articles
-'''
 def get_articles(name):
     get_articles_url=article_url.format(name,api_key)
     with urllib.request.urlopen(get_articles_url) as url:
