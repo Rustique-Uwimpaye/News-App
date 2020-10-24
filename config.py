a1ae774046cc706f355d7fb2be5aa63bb934141d
Rustique-Uwimpaye <rustique644@gmail.com>
2020-10-24 13:00:14 +0300

    config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config:
   
-    NEWS_API_BASE_URL="https://newsapi.org/v2/{}?country=us&apiKey={}"
-    NEWS_BASE_URL="https://newsapi.org/v2/{}?language=en&apiKey={}"
+    NEWS_API_BASE_URL='https://newsapi.org/v2/{}?country=us&apiKey=4010d6ff152744d5853fbb6953696c1f'
+    NEWS_BASE_URL='https://newsapi.org/v2/{}?language=en&apiKey=4010d6ff152744d5853fbb6953696c1f'
     #sources api
-    NEWS_ARTICLE_API="https://newsapi.org/v2/top-headlines?sources={}&apiKey={}"
+    NEWS_ARTICLE_API='https://newsapi.org/v2/top-headlines?sources={}&apiKey=4010d6ff152744d5853fbb6953696c1f'
     NEWS_API_KEY=os.environ.get("NEWS_API_KEY")
     SECRET_KEY=os.environ.get("SECRET_KEY")
 
